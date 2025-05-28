75f589fefcea754832188990ef485a79755790c3
Sarah Qasim <sarahqasim024740@gmail.com>
2025-05-28 18:30:40 +0500

    WIP: local changes to dbConnection.js

--- Zero-Hunger-main/config/dbConnection.js
+++ Zero-Hunger-main/config/dbConnection.js
@@ -2,8 +2,7 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
   try {
-    const db =
-      "mongodb+srv://sarahqasim024740:SaRaH0123@cluster0.5i2nv.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+    const db = process.env.MONGODB_URI;
     await mongoose.connect(db);
     console.log("MongoDB connected...");
   } catch (err) {
